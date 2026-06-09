6aa600f46aceb4c1232a6359bc17560c58d59cea
yaswanthreddypenchikala <yaswanthreddy877@gmail.com>
2026-06-09 10:48:25 +0000

    updated

--- create_instance.sh
+++ create_instance.sh
@@ -6,8 +6,21 @@ INSTANCE_ID=$(
     --instance-type t2.micro \
     --security-group-ids sg-06768f041e8b8b319 \
     --count 1 \
-    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=frontend}]' \
+    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=redis}]' \
     --query 'Instances[0].InstanceId' \
     --output text )
 
 echo "Instance ID : $INSTANCE_ID " 
+
+PRIVATE_IP=$(aws ec2 describe-instances \
+    --instance-ids "$INSTANCE_ID" \
+    --query 'Reservations[0].Instances[0].PrivateIpAddress' \
+    --output text)
+
+PUBLIC_IP=$(aws ec2 describe-instances \
+    --instance-ids "$INSTANCE_ID" \
+    --query 'Reservations[0].Instances[0].PublicIpAddress' \
+    --output text)
+
+echo "Private IP: $PRIVATE_IP"
+echo "Public IP : $PUBLIC_IP"
