ecb80b184485d4fa368ed536b9d369bf15fc57af
yaswanthreddypenchikala <yaswanthreddy877@gmail.com>
2026-06-09 10:41:06 +0000

    updated

--- create_instance.sh
+++ create_instance.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-aws ec2 run-instances \
+INSTANCE_ID=$(
+      aws ec2 run-instances \
     --image-id ami-0220d79f3f480ecf5 \
     --instance-type t2.micro \
     --security-group-ids sg-06768f041e8b8b319 \
     --count 1 \
     --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=frontend}]' \
     --query 'Instances[0].InstanceId' \
-    --output text
+    --output text )
+
+echo "Instance ID : $INSTANCE_ID " 
